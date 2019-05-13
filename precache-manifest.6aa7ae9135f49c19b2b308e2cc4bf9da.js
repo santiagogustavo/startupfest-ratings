5213c2452fc7877c30b9ee957100ec50f60a3102
Gustavo Santiago <santiagogustavo@gmail.com>
2019-05-12 23:50:10 -0300

    Updates

---
+++ precache-manifest.6aa7ae9135f49c19b2b308e2cc4bf9da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "65e286d14f8565e7fb1dd003c1354baa",
+    "url": "/startupfest-ratings/static/media/Root.test.jsx.65e286d1.snap"
+  },
+  {
+    "revision": "48ce5aaeccfdabb18ccafec659cfc3d8",
+    "url": "/startupfest-ratings/static/media/Logo.48ce5aae.png"
+  },
+  {
+    "revision": "cdc15be29927fac5707f",
+    "url": "/startupfest-ratings/static/js/runtime~main.cdc15be2.js"
+  },
+  {
+    "revision": "f45f0686a5436f442933",
+    "url": "/startupfest-ratings/static/js/main.f45f0686.chunk.js"
+  },
+  {
+    "revision": "4808a03f467816c904a6",
+    "url": "/startupfest-ratings/static/js/2.4808a03f.chunk.js"
+  },
+  {
+    "revision": "f45f0686a5436f442933",
+    "url": "/startupfest-ratings/static/css/main.6f1a6a16.chunk.css"
+  },
+  {
+    "revision": "765d9b44bb321c42aadebd574a500890",
+    "url": "/startupfest-ratings/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/startupfest-ratings/precache-manifest.6f0eff9b26f54831447face2b69e1231.js"
+  "/startupfest-ratings/precache-manifest.6aa7ae9135f49c19b2b308e2cc4bf9da.js"
 );
 
 workbox.clientsClaim();
